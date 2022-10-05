6f0b38646ca2e4a21ece305f5ea51704dd6dd341
Gerrishon <scalabli@pm.me>
2022-10-05 18:56:50 -0400

    update

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name="peritia",
+    install_requires=[
+        "colorama; platform_system == 'Windows'",
+        "pygments", 
+    ],
+)
